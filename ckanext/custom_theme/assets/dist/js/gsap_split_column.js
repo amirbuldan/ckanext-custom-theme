9c2eef72b1dc262de1e85c941c3eb0b7adbfe025
amirbuldan <amirbuldan2915@gmail.com>
2025-06-11 08:53:59 +0800

    fix update: add animation hover

--- ckanext/custom_theme/assets/dist/js/gsap_split_column.js
+++ ckanext/custom_theme/assets/dist/js/gsap_split_column.js
@@ -5,23 +5,27 @@ ckan.module('gsap_split_column', function(jQuery) {
     return {
         initialize: function () {
             gsap.registerPlugin(ScrollTrigger);
+            const mm = gsap.matchMedia()
 
             const split_section = document.getElementsByClassName('split-section');
 
             console.log('gsap-split-column initalize....')
             console.log(split_section);
             
-            gsap.to(".split-section", {
-                scrollTrigger: {
-                    trigger: ".split-section",
-                    toggleActions: "restart pause reverse pausu e",
-                    // markers: true,
-                    pin: ".split-section-pinned",
-                    end: "bottom bottom",
-                    ease: "power1.inOut",
-                    duration: 2.5,
-                },
-            });
+            
+            mm.add("(min-width:768px)", () => {
+                gsap.to(".split-section", {
+                    scrollTrigger: {
+                        trigger: ".split-section",
+                        toggleActions: "restart pause reverse pause",
+                        // markers: true,
+                        pin: ".split-section-pinned",
+                        end: "bottom bottom",
+                        ease: "power1.inOut",
+                        duration: 2.5,
+                    },
+                });
+            })
 
         }
     };
